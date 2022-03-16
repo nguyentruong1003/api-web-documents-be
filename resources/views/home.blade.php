e8dd6315b42e6d5514b9b201fa40676acef8e169
Nguyễn Văn Trường <nguyenvantruong2k@gmail.com>
2022-03-16 17:39:05 +0700

    update library

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.master')
-
+@section('title')
+    <title>Admin Dashboard</title>
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
